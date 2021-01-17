3a8aec9210dbe0ef5ab9d462e90380edae51c784
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-01-17 12:05:30 -0300

    replaced function views with class based views

--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'catalog/home.html'
+
+
+class SearchResultsView(TemplateView):
+    template_name = 'catalog/search_results.html'
